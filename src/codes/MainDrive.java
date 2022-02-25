9f491ce9e8c7f48b3c860a43d8da0f456c7f4427
이진실 <ljsil1204@naver.com>
2022-02-25 15:45:34 +0900

    중복 숫자가 아니라면, 문제로 선정

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -18,8 +18,27 @@ public class MainDrive {
 				Random myRandom = new Random();
 				int randomNum = myRandom.nextInt(9)+1; // 1~10직전 (9)
 				
+				boolean isDulpOk = true;
+				
+				for(int num : cpuNumbers) {
+					if (num == randomNum) {
+						isDulpOk = false;
+						break;
+					}
+				}
+				
+				if (isDulpOk) {
+					cpuNumbers[i] = randomNum;
+					break;
+				}
+				
 			}
 			
+		}
+		
+//		문제 확인용 for문
+		for(int num : cpuNumbers) {
+			System.out.print(num);
 		}
 		
 	}
